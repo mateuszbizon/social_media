5c7de514cfd888e79e619f07d42e675f3d51bd7a
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-10 15:48:27 +0200

    added h1 gradient for sidebar

--- src/components/common/Sidebar.tsx
+++ src/components/common/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Sidebar() {
   return (
     <aside className='flex flex-col w-[250px] h-screen sticky top-0 bg-white border-r border-r-gray-2 px-3 py-10'>
-        <h1 className='text-2xl font-semibold'>Matgram</h1>
+        <h1 className='text-2xl font-semibold gradient-text'>Matgram</h1>
         <ul className='mt-10'>
             <li>
                 Home
