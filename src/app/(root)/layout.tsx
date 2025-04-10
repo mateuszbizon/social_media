6b51d38ac518e1f108c7244d40815a1ef221c8f9
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-10 15:47:11 +0200

    added sidebar

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,5 @@
+import Sidebar from "@/components/common/Sidebar";
+
 export default function RootLayout({
     children,
   }: Readonly<{
@@ -5,9 +7,7 @@ export default function RootLayout({
   }>) {
     return (
       <main className="flex">
-        <aside className="w-[300px] h-screen sticky top-0 bg-white">
-
-        </aside>
+        <Sidebar />
         <div className="grow">
             <div className="w-full max-w-[900px] px-5 mx-auto">
                 {children}
---
+++ src/components/common/Sidebar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <aside className='flex flex-col w-[250px] h-screen sticky top-0 bg-white border-r border-r-gray-2 px-3 py-10'>
+        <h1 className='text-2xl font-semibold'>Matgram</h1>
+        <ul className='mt-10'>
+            <li>
+                Home
+            </li>
+            <li>
+                Search
+            </li>
+        </ul>
+        <div className='mt-auto'>
+            <button>logout</button>
+        </div>
+    </aside>
+  )
+}
+
+export default Sidebar
