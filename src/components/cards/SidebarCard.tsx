86012e315cff5ea1ab8a20bc1d40e8e31304677f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-11 18:09:16 +0200

    added current path check for sidebar links

--- src/components/cards/SidebarCard.tsx
+++ src/components/cards/SidebarCard.tsx
@@ -3,6 +3,7 @@ import { Button } from '../ui/button'
 import Link from 'next/link'
 import { useSidebar } from '@/context/SidebarContext'
 import { SidebarItem } from '@/types'
+import { usePathname } from 'next/navigation'
 
 type SidebarCardProps = {
     item: SidebarItem
@@ -10,12 +11,14 @@ type SidebarCardProps = {
 
 function SidebarCard({ item }: SidebarCardProps) {
     const { isOpen, chooseAction } = useSidebar()
+    const pathname = usePathname()
+    const currentPath = item.isLink && pathname.includes(item.href)
 
   return (
     <>
         {item.isLink ? (
             <Button variant={"transparent"} className='w-full justify-start' asChild>
-                <Link href={`${item.href}`}>{item.icon}{isOpen && <span>{item.name}</span>}</Link>
+                <Link href={`${item.href}`}>{item.icon}{isOpen && <span className={`${currentPath && "font-bold"}`}>{item.name}</span>}</Link>
             </Button>
         ) : (
             <Button variant={"transparent"} className='w-full justify-start' onClick={() => chooseAction(item.action)}>
