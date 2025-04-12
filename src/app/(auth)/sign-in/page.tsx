c965cb97b6671c534a90f6a877734eda801ef85a
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-12 12:37:25 +0200

    added sign in form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -4,6 +4,9 @@ function SignInPage() {
   return (
     <>
         <h1 className='heading1 gradient-text text-center mb-10'>Matgram</h1>
+        <div className='w-full max-w-[400px]'>
+            <h2 className='heading2 text-center text-black-2 mb-5'>Sign in</h2>
+        </div>
     </>
   )
 }
---
+++ src/components/forms/SignInForm.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+function SignInForm() {
+  return (
+    <form>
+        
+    </form>
+  )
+}
+
+export default SignInForm
