36ba6a6238e89a22b7de3a73b46210d5b4cce3e3
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-24 11:01:52 +0200

    added get post service

--- src/lib/services/posts.ts
+++ src/lib/services/posts.ts
@@ -1,8 +1,22 @@
-import { GetUserPostsResponse } from "@/types/postResponse";
+import { GetPostResponse, GetUserPostsResponse } from "@/types/postResponse";
 import { API } from ".";
+import { ServiceResult } from "@/types";
+import { handleApiError } from "../utils";
 
 export async function getUserPosts(page: number, sort: string, userId: string) {
     const { data } = await API.get<GetUserPostsResponse>(`/post/get-user-posts/${userId}?page=${page}&sort=${sort}`)
 
     return data
+}
+
+export async function getSinglePost(postId: string): Promise<ServiceResult<GetPostResponse>> {
+    try {
+        const { data } = await API.get(`/post/get-post/${postId}`)
+
+        return {
+            data
+        }
+    } catch (error) {
+        return handleApiError(error)
+    }
 }
