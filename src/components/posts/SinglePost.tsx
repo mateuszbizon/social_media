c4be825a6bda8da475ddfea7c89bc2e609ff919b
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-29 11:02:02 +0200

    moved likes and comments below content and image

--- src/components/posts/SinglePost.tsx
+++ src/components/posts/SinglePost.tsx
@@ -10,32 +10,43 @@ type SinglePostProps = GetPostResponse
 
 function SinglePost({ post, author, commentsCount, likes }: SinglePostProps) {
   return (
-    <div className={`grid gap-5 ${post.image && "md:grid-cols-2"} rounded-2xl p-5 border border-gray-2`}>
-        <div className='space-y-5'>
-            <div className='flex justify-between gap-2'>
-                <div className='flex items-center space-x-2'>
-                    <Link href={`/user/${author.username}`} target='_blank'>
-                        <div className='relative size-12 rounded-full overflow-hidden'>
-                            <Image src={author.avatar ?? "/user_empty.jpg"} alt='Author image' fill className='object-cover' />
-                        </div>
-                    </Link>
-                    <div className='space-y-1'>
+    <div className='space-y-5 rounded-2xl p-5 border border-gray-2'>
+        <div className={`grid gap-5 ${post.image && "md:grid-cols-2"}`}>
+            <div className='space-y-5'>
+                <div className='flex justify-between gap-2'>
+                    <div className='flex items-center space-x-2'>
                         <Link href={`/user/${author.username}`} target='_blank'>
-                            <p className='text-black-2 font-medium line-clamp-1'>{author.username}</p>
+                            <div className='relative size-12 rounded-full overflow-hidden'>
+                                <Image src={author.avatar ?? "/user_empty.jpg"} alt='Author image' fill className='object-cover' />
+                            </div>
                         </Link>
-                        <p className='text-gray-2 line-clamp-1 text-sm'>{post.createdAt.toString()}</p>
+                        <div className='space-y-1'>
+                            <Link href={`/user/${author.username}`} target='_blank'>
+                                <p className='text-black-2 font-medium line-clamp-1'>{author.username}</p>
+                            </Link>
+                            <p className='text-gray-2 line-clamp-1 text-sm'>{post.createdAt.toString()}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
 
-            <p className='font-medium text-black-2'>{post.content}</p>
-            
+                <p className='font-medium text-black-2'>{post.content}</p>
+                
+                {post.image && (
+                    <div className='relative w-full aspect-video rounded-2xl overflow-hidden md:hidden'>
+                        <Image src={post.image} alt='Post image' fill className='object-cover' />
+                    </div>
+                )}
+            </div>
             {post.image && (
-                <div className='relative w-full aspect-video rounded-2xl overflow-hidden md:hidden'>
-                    <Image src={post.image} alt='Post image' fill className='object-cover' />
+                <div className='hidden md:block'>
+                    <div className='relative w-full max-w-[400px] mx-auto aspect-video rounded-2xl overflow-hidden'>
+                        <Image src={post.image} alt='Post image' fill className='object-cover' />
+                    </div>
                 </div>
             )}
+        </div>
 
+        <div className='space-y-5'>
             <div className='flex items-center gap-5'>
                 <PostLike likes={likes} postId={post.id} />
                 <div className='flex gap-3 items-center'>
@@ -46,13 +57,6 @@ function SinglePost({ post, author, commentsCount, likes }: SinglePostProps) {
 
             <PostComments postId={post.id} />
         </div>
-        {post.image && (
-            <div className='hidden md:block'>
-                <div className='relative w-full max-w-[400px] mx-auto aspect-video rounded-2xl overflow-hidden'>
-                    <Image src={post.image} alt='Post image' fill className='object-cover' />
-                </div>
-            </div>
-        )}
     </div>
   )
 }
