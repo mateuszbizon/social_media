6a32971041c715b80176c75c9ace5d8723d829d8
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-04-30 10:40:57 +0200

    added create post page

---
+++ src/app/(root)/create-post/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function CreatePostPage() {
+  return (
+    <div>
+        <h2 className='heading2 text-black-2 mb-5'>Create Post</h2>
+        <div className='border border-gray-2 rounded-xl p-5 shadow-xl'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default CreatePostPage
