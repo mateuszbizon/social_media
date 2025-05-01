4324c7ffa1fc483ad21638c082fb31a4ebd50576
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-01 17:25:07 +0200

    added update post hook to post form

--- src/lib/hooks/services/posts/useGetBasicPost.ts
+++ src/lib/hooks/services/posts/useGetBasicPost.ts
@@ -8,7 +8,8 @@ type Props = {
 function useGetBasicPost({ postId }: Props) {
     const { data, isError, error, isPending } = useQuery({
         queryKey: ['getBasicPost', postId],
-        queryFn: () => getBasicPost(postId)
+        queryFn: () => getBasicPost(postId),
+        gcTime: 0
     })
 
   return {
