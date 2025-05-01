fd1e19c357b3cfadd8fff4c1e679ad11c5515160
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-01 20:18:22 +0200

    added delete post custom hook

---
+++ src/lib/hooks/services/posts/useDeletePost.ts
@@ -0,0 +1,27 @@
+import { MESSAGES } from '@/constants/messages'
+import { deletePost } from '@/lib/services/posts'
+import { ErrorResponse } from '@/types'
+import { useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-toastify'
+
+function useDeletePost() {
+    const router = useRouter()
+    const { mutateAsync: handleDeletePost } = useMutation({
+        mutationFn: deletePost,
+        onSuccess: () => {
+            router.back()
+            toast.success(MESSAGES.post.deleted)
+        },
+        onError: (error: AxiosError<ErrorResponse>) => {
+            toast.error(error.response?.data.message ?? MESSAGES.network.fail)
+        }
+    })
+
+  return {
+    handleDeletePost,
+  }
+}
+
+export default useDeletePost
