c81bce9b2101c454d22d05ecf640a9cac41aa8e4
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-01 12:47:39 +0200

    used get basic post hook

--- src/app/(root)/edit-post/[id]/page.tsx
+++ src/app/(root)/edit-post/[id]/page.tsx
@@ -1,6 +1,9 @@
 "use client"
 
 import withAuth from '@/components/auth/withAuth'
+import MainError from '@/components/errors/MainError'
+import CircleLoading from '@/components/ui/circleLoading'
+import useGetBasicPost from '@/lib/hooks/services/posts/useGetBasicPost'
 import { User } from '@/types/models'
 import { useParams } from 'next/navigation'
 import React from 'react'
@@ -11,6 +14,15 @@ type Props = {
 
 function EditPostPage({ user }: Props) {
     const { id } = useParams<{ id: string }>()
+    const { data, isError, error, isPending } = useGetBasicPost({
+        postId: id
+    })
+
+    if (isPending) return <CircleLoading className='mx-auto' />
+
+    if (isError) return <MainError message={error?.message || ""} />
+
+    if (data?.authorId !== user.id) return <MainError message='Forbidden' />
 
   return (
     <div>EditPostPage</div>
