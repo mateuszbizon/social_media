8d1262efcffbc66b3ba2689b2065e3e52eaf5141
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-02 18:44:18 +0200

    added delete dialog

--- src/components/comments/PostCommentDelete.tsx
+++ src/components/comments/PostCommentDelete.tsx
@@ -1,9 +1,10 @@
 "use client"
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '../ui/button'
 import { Trash2 } from 'lucide-react'
 import { useAuthContext } from '@/context/AuthContext'
+import { Dialog, DialogClose, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from '../ui/dialog'
 
 type PostCommentDeleteProps = {
     commentId: string
@@ -12,15 +13,34 @@ type PostCommentDeleteProps = {
 
 function PostCommentDelete({ commentId, authorId }: PostCommentDeleteProps) {
     const { isAuthor } = useAuthContext()
+    const [deleteCommentOpen, setDeleteCommentOpen] = useState(false)
 
     if (!isAuthor(authorId)) return null
 
   return (
-    <>
-        <Button variant={"transparent"} size={"link"} className='hover:bg-transparent text-red-2 hover:text-red-3'>
-            <Trash2  />
-        </Button>
-    </>
+    <Dialog open={deleteCommentOpen} onOpenChange={setDeleteCommentOpen}>
+        <DialogTrigger asChild>
+            <Button variant={"transparent"} size={"link"} className='hover:bg-transparent text-red-2 hover:text-red-3'>
+                <Trash2  />
+            </Button>
+        </DialogTrigger>
+        <DialogContent>
+            <DialogHeader>
+                <DialogTitle>Are you sure you want to delete this comment?</DialogTitle>
+                <DialogDescription>This action cannot be undone. This will permanently delete your comment.</DialogDescription>
+            </DialogHeader>
+            <DialogFooter className='gap-3 sm:justify-center'>
+                <DialogClose asChild>
+                    <Button variant={"outline"} size={"sm"}>
+                        Cancel
+                    </Button>
+                </DialogClose>
+                <Button variant={"destructive"} size={"sm"}>
+                    Delete
+                </Button>
+            </DialogFooter>
+        </DialogContent>
+    </Dialog>
   )
 }
 
