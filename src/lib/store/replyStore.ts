8249d5c264a5dacdfd509dc7e32ed96625275c15
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-05 13:15:27 +0200

    added zustand with reply store

---
+++ src/lib/store/replyStore.ts
@@ -0,0 +1,21 @@
+import { create } from "zustand"
+
+type Reply = {
+    commentId: string
+    replyingToId: string
+    replyingTo: string
+}
+
+export type ReplyStore = {
+    reply: Reply | null
+    setReply: (reply: Reply | null) => void
+    clearReply: () => void
+}
+
+const useReplyStore = create<ReplyStore>()(set => ({
+    reply: null,
+    setReply: (reply) => set({ reply }),
+    clearReply: () => set({ reply: null })
+}))
+
+export default useReplyStore
