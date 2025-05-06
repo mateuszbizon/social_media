829b4ecf383c90116061d67dd07f420acffcf22d
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-06 10:39:26 +0200

    added create comment reply custom hook

---
+++ src/lib/hooks/services/replies/useCreateCommentReply.ts
@@ -0,0 +1,47 @@
+import { MESSAGES } from '@/constants/messages'
+import { createCommentReply } from '@/lib/services/replies'
+import { ErrorResponse } from '@/types'
+import { GetCommentRepliesResponse } from '@/types/replyResponse'
+import { InfiniteData, QueryClient, useMutation } from '@tanstack/react-query'
+import { AxiosError } from 'axios'
+import { toast } from 'react-toastify'
+
+type Props = {
+    commentId: string
+}
+
+function useCreateCommentReply({ commentId }: Props) {
+    const queryClient = new QueryClient()
+
+    const { mutateAsync: handleCreateCommentReply } = useMutation({
+        mutationFn: createCommentReply,
+        onSuccess: (data) => {
+            queryClient.setQueryData<InfiniteData<GetCommentRepliesResponse>>(['getCommentReplies', commentId], (oldData) => {
+                if (!oldData) return oldData
+
+                return {
+                    ...oldData,
+                    pages: oldData.pages.map((page, index) => {
+                        if (index == 0) {
+                            return {
+                                ...page,
+                                replies: [data.reply, ...page.replies]
+                            }
+                        }
+
+                        return page
+                    })
+                }
+            })
+        },
+        onError: (error: AxiosError<ErrorResponse>) => {
+            toast.error(error.response?.data.message ?? MESSAGES.network.fail)
+        }
+    })
+
+  return {
+    handleCreateCommentReply,
+  }
+}
+
+export default useCreateCommentReply
