b8b3c4af077fc08d1d874286e60595bd81727270
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-06 14:13:18 +0200

    added comment reply form to comment card

--- src/components/cards/PostCommentCard.tsx
+++ src/components/cards/PostCommentCard.tsx
@@ -11,6 +11,7 @@ import PostCommentLike from '../comments/PostCommentLike'
 import CommentReplies from '../replies/CommentReplies'
 import { ChevronDown } from 'lucide-react'
 import PostCommentReply from '../comments/PostCommentReply'
+import CommentReplyForm from '../forms/CommentReplyForm'
 
 type PostCommentCardProps = {
     comment: PostComment
@@ -19,54 +20,58 @@ type PostCommentCardProps = {
 function PostCommentCard({ comment }: PostCommentCardProps) {
     const [likesCount, setLikesCount] = useState(comment.likes.length)
     const [showReplies, setshowReplies] = useState(false)
+    const [replyFormShow, setReplyFormShow] = useState(false)
+    
+    function toggleReplyForm() {
+        setReplyFormShow(prev => !prev)
+    }
+
+    function closeReplyForm() {
+        setReplyFormShow(false)
+    }
 
   return (
-    <div className='flex justify-between items-center'>
-        <div className='flex space-x-2'>
-            <div>
-                <Link href={`/user/${comment.author.username}`} target='_blank'>
-                    <div className='relative size-10 rounded-full overflow-hidden'>
-                        <Image src={comment.author.avatar ?? "/user_empty.jpg"} alt='Author image' fill className='object-cover' />
-                    </div>
-                </Link>
-            </div>
-
-            <div className='space-y-1'>
-                <p className='space-x-2 text-black-2 text-sm'>
-                    <Link href={`/user/${comment.author.username}`} target='_blank' className={`${buttonVariants({ variant: "link", size: "link" })} text-sm`}>
-                        <span>{comment.author.username}</span>
-                    </Link>
-                    <span>
-                        {comment.content}
-                    </span>
-                </p>
-                <div className='flex gap-3 text-gray-2 text-sm'>
-                    <span>{moment(comment.createdAt.toString()).fromNow()}</span>
-                    <span className='font-medium'>{likesCount} likes</span>
-                    <PostCommentLike likes={comment.likes} setLikesCount={setLikesCount} commentId={comment.id} />
-                    {/* <PostCommentReply 
-                        reply={{
-                            commentId: comment.id,
-                            replyingToId: comment.author.id,
-                            replyingTo: comment.author.username
-                        }}
-                    /> */}
-                    <PostCommentDelete commentId={comment.id} authorId={comment.author.id} />
+    <div className='flex space-x-2'>
+        <div>
+            <Link href={`/user/${comment.author.username}`} target='_blank'>
+                <div className='relative size-10 rounded-full overflow-hidden'>
+                    <Image src={comment.author.avatar ?? "/user_empty.jpg"} alt='Author image' fill className='object-cover' />
                 </div>
-                {comment.replyCount > 0 && (
-                    <>
-                        {showReplies ? (
-                            <div className='mt-3'>
-                                <CommentReplies commentId={comment.id} />
-                            </div>
-                        ) : (
-                            <Button variant={"transparent"} size={"sm"} className='text-black-2' onClick={() => setshowReplies(true)}>
-                                Show replies ({comment.replyCount}) <ChevronDown />
-                            </Button>
-                        )}
-                    </>
-                )}
+            </Link>
+        </div>
+
+        <div className='space-y-1'>
+            <p className='space-x-2 text-black-2 text-sm'>
+                <Link href={`/user/${comment.author.username}`} target='_blank' className={`${buttonVariants({ variant: "link", size: "link" })} text-sm`}>
+                    <span>{comment.author.username}</span>
+                </Link>
+                <span>
+                    {comment.content}
+                </span>
+            </p>
+            <div className='flex gap-3 text-gray-2 text-sm'>
+                <span>{moment(comment.createdAt.toString()).fromNow()}</span>
+                <span className='font-medium'>{likesCount} likes</span>
+                <PostCommentLike likes={comment.likes} setLikesCount={setLikesCount} commentId={comment.id} />
+                <PostCommentReply toggleReplyForm={toggleReplyForm} />
+                <PostCommentDelete commentId={comment.id} authorId={comment.author.id} />
             </div>
+            <div className={`${replyFormShow ? "block" : "hidden"}`}>
+                <CommentReplyForm commentId={comment.id} replyingToId={comment.author.id} closeReplyForm={closeReplyForm} />
+            </div>
+            {comment.replyCount > 0 && (
+                <>
+                    {showReplies ? (
+                        <div className='mt-3'>
+                            <CommentReplies commentId={comment.id} />
+                        </div>
+                    ) : (
+                        <Button variant={"transparent"} size={"sm"} className='text-black-2' onClick={() => setshowReplies(true)}>
+                            Show replies ({comment.replyCount}) <ChevronDown />
+                        </Button>
+                    )}
+                </>
+            )}
         </div>
     </div>
   )
