abe60238bbfd81d50653bf15c03ba28d7c44aa98
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-08 17:51:56 +0200

    added liked posts page

---
+++ src/app/(root)/liked-posts/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import withAuth from '@/components/auth/withAuth'
+import React from 'react'
+
+function LikedPostsPage() {
+  return (
+    <div>LikedPostsPage</div>
+  )
+}
+
+export default withAuth(LikedPostsPage)
