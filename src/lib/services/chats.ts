5d8c5672e8ef6e36696f55f1501c098a90c2f156
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-16 15:17:39 +0200

    added create chat service

--- src/lib/services/chats.ts
+++ src/lib/services/chats.ts
@@ -1,8 +1,14 @@
-import { GetChatsResponse } from "@/types/chatResponse";
+import { CreateChatResponse, GetChatsResponse } from "@/types/chatResponse";
 import { API } from ".";
 
 export async function getChats(page: number) {
     const { data } = await API.get<GetChatsResponse>(`/chats/get-chats?page=${page}`)
+
+    return data
+}
+
+export async function createChat(userIds: string[]) {
+    const { data } = await API.post<CreateChatResponse>("/chats/create-chat", userIds)   
 
     return data
 }
