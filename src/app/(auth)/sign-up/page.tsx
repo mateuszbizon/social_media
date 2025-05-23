489dc3e7223ffd63f7a463b12f4b7ccfc69b0d24
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-23 19:10:26 +0200

    added links to sign in/sign up pages

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,4 +1,6 @@
 import SignUpForm from '@/components/forms/SignUpForm'
+import { buttonVariants } from '@/components/ui/button'
+import Link from 'next/link'
 import React from 'react'
 
 function SignUpPage() {
@@ -8,6 +10,12 @@ function SignUpPage() {
         <div className='basic-form-container'>
             <h2 className='heading2 text-center text-black-2 mb-5'>Sign up</h2>
             <SignUpForm />
+            <p className='text-center text-black-2 mt-5'>
+                Already have account? 
+                <Link href={"/sign-in"} className={`ml-2 ${buttonVariants({ variant: "link", size: "link" })}`}>
+                    Sign in
+                </Link>
+            </p>
         </div>
     </div> 
   )
