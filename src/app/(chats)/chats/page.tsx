a780d3e662ad183a3ab2d97d2c0c20266300ba96
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-23 11:13:06 +0200

    displaying one chat element at once on mobile

--- src/app/(chats)/chats/page.tsx
+++ src/app/(chats)/chats/page.tsx
@@ -3,13 +3,32 @@
 import withAuth from '@/components/auth/withAuth'
 import Chats from '@/components/chats/Chats'
 import SingleChat from '@/components/chats/SingleChat'
-import React from 'react'
+import useChatStore from '@/store/chatStore'
+import React, { useEffect, useState } from 'react'
 
 function ChatsPage() {
+    const { selectedChat } = useChatStore()
+    const [width, setWidth] = useState(window.innerWidth)
+    const isMobile = width < 768
+
+    const handleResize = () => {
+        setWidth(window.innerWidth)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
   return (
     <div className='flex'>
-        <Chats />
-        <div className='grow'>
+        <div className={`${isMobile ? selectedChat ? "hidden" : "w-full" : "block"}`}>
+            <Chats />
+        </div>
+        <div className={`${isMobile ? selectedChat ? "block" : "hidden" : "block"} grow`}>
             <SingleChat />
         </div>
     </div>
