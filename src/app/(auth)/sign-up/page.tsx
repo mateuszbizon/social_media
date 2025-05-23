87537ae84b8e651ee849b7a5f5ca942bad34e93f
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-23 16:25:58 +0200

    added sign up page

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SignUpPage() {
+  return (
+    <div>
+        <h1 className='heading1 gradient-text text-center mb-10'>Matgram</h1>
+        <div className='basic-form-container'>
+            <h2 className='heading2 text-center text-black-2 mb-5'>Sign up</h2>
+        </div>
+    </div> 
+  )
+}
+
+export default SignUpPage
